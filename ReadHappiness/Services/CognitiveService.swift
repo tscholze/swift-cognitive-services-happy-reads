85578e6db930d26802d76f1fba236261b90ad1ed
Tobias Scholze <tobias@Tobiass-MBP.fritz.box>
2016-11-24 22:09:38 +0100

    Added logo and text to splashscreen

--- ReadHappiness/Services/CognitiveService.swift
+++ ReadHappiness/Services/CognitiveService.swift
@@ -22,7 +22,8 @@ class CognitiveService
     // MARK: - Private properties -
     
     fileprivate static let ApiKey = "2049060041db4bc5960e0acf40391ae2"
-    fileprivate static let ApiUrl = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment"    
+    fileprivate static let ApiUrl = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment"
+    
     
     // MARK: - Life cycle -
     
